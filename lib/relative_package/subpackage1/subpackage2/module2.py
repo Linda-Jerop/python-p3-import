0d4dcda8a8a5c7101fda153ee74e8c654bc9f6c7
Linda <jeropkoimur@gmail.com>
2025-11-20 11:52:52 +0300

    import

--- lib/relative_package/subpackage1/subpackage2/module2.py
+++ lib/relative_package/subpackage1/subpackage2/module2.py
@@ -1,5 +1,5 @@
-from .module1 import function1
-# from .. import module3
+from .module1 import function1  # imports function1 from module1 in same directory
+from .. import module3  # imports module3 from parent directory (subpackage1)
 
-function1()
-# module3.function1()
+function1()  # calls function1 from module1
+module3.function1()  # calls function1 from module3
